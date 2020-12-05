6e0c4bf18aea01b5ea631e28b34cb857ecc84856
faiyuching <faiyuching@gmail.com>
2020-12-05 14:38:07 +0800

    update

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -5,7 +5,7 @@ export default ({ req }) => {
   if (typeof window === 'undefined') {
     // We are on the server
     return axios.create({
-      baseURL: 'http://www.zhuominginfo.org/',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
